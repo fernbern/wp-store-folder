ea01b01b62d8d687e8c098c5f1094b49c13a108f
fernbern <70728008+fernbern@users.noreply.github.com>
2021-03-30 13:40:14 -0700

    Add files via upload

---
+++ newtheme/functions.php
@@ -0,0 +1,5 @@
+<?php 
+function load_functions() {
+    wp_enqueue('array');
+}
+?>
---
+++ newtheme/index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<h1>Fernando Castanon Hello World</h1>
+<div>
+<div class="container">
+<h1>hello world</h1></div>
+</div>
+<?php get_footer(); ?>
